d7dbabd8190ebd890329ed2d5aae2596f9c62049
infernus255 <eros_gabriel@live.com.ar>
2020-12-29 18:40:56 -0300

    Add files via upload

--- CodingChallenge.Data.Tests/DataTests.cs
+++ CodingChallenge.Data.Tests/DataTests.cs
@@ -8,41 +8,60 @@ namespace CodingChallenge.Data.Tests
     [TestFixture]
     public class DataTests
     {
+        ImpresoraTraductoraFormas impresora;
+        Traductor traductor;
+        [SetUp]
+        public void Init()
+        {
+            impresora = new ImpresoraTraductoraFormas();
+            //agregar palabras al dic
+            traductor = new Traductor();
+            traductor.agregarTraduccion("List", "Lista", "spanish");
+            traductor.agregarTraduccion("empty", "vacía", "spanish");
+            traductor.agregarTraduccion("of", "de", "spanish");
+            traductor.agregarTraduccion("shapes", "formas", "spanish");
+            traductor.agregarTraduccion("Report", "Reporte", "spanish");
+            traductor.agregarTraduccion("Report", "Reporte", "spanish");
+            traductor.agregarTraduccion("Shapes", "Formas", "spanish");
+            traductor.agregarTraduccion("Square", "Cuadrado", "spanish");
+            traductor.agregarTraduccion("Squares", "Cuadrados", "spanish");
+            traductor.agregarTraduccion("Circle", "Círculo", "spanish");
+            traductor.agregarTraduccion("Circles", "Círculos", "spanish");
+            traductor.agregarTraduccion("Rectangle", "Rectangulo", "spanish");
+            traductor.agregarTraduccion("Rectangles", "Rectangulos", "spanish");
+            traductor.agregarTraduccion("Perimeter", "Perimetro", "spanish");
+            traductor.agregarTraduccion("Area", "Area", "spanish");
+            traductor.agregarTraduccion("TOTAL", "TOTAL", "spanish");
+        }
+
         [TestCase]
         public void TestResumenListaVacia()
         {
-            Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 1));
+            Assert.AreEqual("<h1>Lista vacía de formas!</h1>", impresora.imprimir(traductor,"spanish"));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
             Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 2));
+                impresora.imprimir(traductor, "spanish"));
         }
 
         [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            var cuadrados = new List<FormaGeometrica> {new FormaGeometrica(FormaGeometrica.Cuadrado, 5)};
-
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Castellano);
-
+            impresora.agregarForma(new Cuadrado(5));
+            string resumen = impresora.imprimir(traductor, "spanish");
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
         }
 
         [TestCase]
         public void TestResumenListaConMasCuadrados()
         {
-            var cuadrados = new List<FormaGeometrica>
-            {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 1),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 3)
-            };
-
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Ingles);
+            impresora.agregarForma(new Cuadrado(5));
+            impresora.agregarForma(new Cuadrado(1));
+            impresora.agregarForma(new Cuadrado(3));
+            string resumen = impresora.imprimir(traductor, "english");
 
             Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
@@ -50,18 +69,16 @@ namespace CodingChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConMasTipos()
         {
-            var formas = new List<FormaGeometrica>
-            {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
-            };
+            impresora.agregarForma(new Cuadrado(5));
+            impresora.agregarForma(new Circulo(3));
+            impresora.agregarForma(new TrianguloEquilatero(4));
+            impresora.agregarForma(new Cuadrado(2));
+            impresora.agregarForma(new TrianguloEquilatero(9));
+            impresora.agregarForma(new Circulo(2.75m));
+            impresora.agregarForma(new TrianguloEquilatero(4.2m));
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Ingles);
+
+            string resumen = impresora.imprimir(traductor, "english");
 
             Assert.AreEqual(
                 "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
@@ -71,18 +88,16 @@ namespace CodingChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConMasTiposEnCastellano()
         {
-            var formas = new List<FormaGeometrica>
-            {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
-            };
+            impresora.agregarForma(new Cuadrado(5));
+            impresora.agregarForma(new Circulo(3));
+            impresora.agregarForma(new TrianguloEquilatero(4));
+            impresora.agregarForma(new Cuadrado(2));
+            impresora.agregarForma(new TrianguloEquilatero(9));
+            impresora.agregarForma(new Circulo(2.75m));
+            impresora.agregarForma(new TrianguloEquilatero(4.2m));
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Castellano);
+
+            string resumen = impresora.imprimir(traductor, "spanish");
 
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
